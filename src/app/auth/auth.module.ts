c093899f6780dde0a6b9ae93c2d781de8fb9d2cc
Youssef Laaroussi <yousseflaar17@gmail.com>
2025-09-03 19:52:28 +0100

    add routing implementation

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,15 +2,38 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {ForgotPasswordComponent} from "./forgot-password/forgot-password.component";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+    VerifyEmailComponent,
+    OAuth2RedirectComponent
+
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    ReactiveFormsModule,
+    RouterModule.forChild([
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'forgot-password', component: ForgotPasswordComponent },
+      { path: 'reset-password', component: ResetPasswordComponent },
+      { path: 'verify-email', component: VerifyEmailComponent },
+      { path: 'oauth2/redirect', component: OAuth2RedirectComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ])
   ]
 })
+
 export class AuthModule {
 
 }
